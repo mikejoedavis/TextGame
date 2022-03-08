de3cd2029da9e038661f66ae4ba2faf5eee8028e
mikejoedavis <101206112+mikejoedavis@users.noreply.github.com>
2022-03-08 18:27:53 +0000

    Add files via upload

---
+++ Scripts/State.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+
+public class State : ScriptableObject
+{
+
+    [TextArea(15, 20)] [SerializeField] string StoryText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return StoryText;
+    }
+
+  public State[] getNextStates()
+    {
+        return nextStates;
+    }
+
+
+
+
+}
---
+++ Scripts/ThiefGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThiefGame : MonoBehaviour
+{
+    [SerializeField] Text TextComponent = default;
+    [SerializeField] State StartingState;
+
+    State currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = StartingState;
+        TextComponent.text = currentState.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manageState();
+
+        
+    }
+
+    private void manageState()
+    {
+        var storyoptions = currentState.getNextStates();
+        
+        for(int index = 0; index < storyoptions.Length; index ++)
+
+        if (Input.GetKeyDown(KeyCode.Alpha1+index))
+        {
+            currentState = storyoptions[index];
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+                currentState = StartingState;
+        }
+
+        TextComponent.text = currentState.GetStateStory();
+    }
+}
